ecf9d636ef8eb321a8337bee6bc68566058a68f4
DamnIgorNK <agadeem787@gmail.com>
2022-01-02 11:51:15 +0900

    second commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import config
 import tweepy
 from collections import Counter
 import numpy as np
+import time
 
 # 保存ディレクトリ
 data_dir = '../data/'
@@ -22,29 +23,38 @@ auth = tweepy.OAuthHandler(api_key,api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-
-#天気情報
-# WEATHER_DICT = {'晴':'Sunny',
-#                 '曇':'Cloudy',
-#                 'くもり':'Cloudy',
-#                 '雨':'Rainy',
-#                 '雷':'Thunder',
-#                 '雪':'Snowy'}
-
+WEATHER_LIST = {'晴':0,
+                '曇':0,
+                'くもり':0,
+                '雨降':0,
+                '雷':0,
+                '雪降':0}
 
 def main():
-    #入力する地名
+    # 対象の地名
     search_keyword = input('地名を入力してください：')
-    item_num = 6 # 6個くらい拾ってくれりゃいける
+    
+    # 検索条件
+    q = f'{search_keyword} 晴 OR くもり OR 曇 OR 雨降 OR 雪降 OR 雷'
+    item_num = 10
 
-    tweets = tweepy.Cursor(api.search_tweets, q=f'{search_keyword} 晴 OR くもり OR 曇 OR 雨 OR 雪 OR 雷', lang='ja').items(item_num)
+    # 計算開始時間
+    start_time = time.time()
+
+    tweets = tweepy.Cursor(api.search_tweets, q=q, lang='ja').items(item_num)
+
     for tweet in tweets:
-        print(tweet.text)
-        print(tweet.id)
+        for weather in WEATHER_LIST.keys():
+            if weather in tweet.text: WEATHER_LIST[weather]+=1
+    
+    # 総計算時間
+    elapsed_time=time.time()-start_time
+    print(f'計算時間：{elapsed_time}sec')
+
+    print(f'{time.time()}時点での{search_keyword}の天気は{max(WEATHER_LIST,key=WEATHER_LIST.get)}であると見込まれます．')
 
 if __name__=='__main__':
     main()
-
 
 
 
